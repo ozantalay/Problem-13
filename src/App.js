fa7984bcc72b16db84628cd0b790319886c37022
ozantalay <ozntalay@hotmail.com>
2024-08-13 21:53:05 +0300

    problem13 Final

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import { useState } from 'react'
 // Formun yanında adı, soyadı ve saat dilimini görüntüleyin
 // Bonus: Herhangi bir değer eksikse kaydet butonunu devre dışı bırakalım
 export default function PersonalInfo() {
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [timezone, setTimezone] = useState('');
+
+  const isFormComplete = firstName && lastName && timezone;
+
+
   return (
     <div className='bg-gray-800 h-screen divide-y divide-white/5'>
       <div className='grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 py-16 sm:px-6 md:grid-cols-3 lg:px-8'>
         <div>
           <h2 className='text-base font-semibold leading-7 text-white'>
-            Adı Soyadı
+            {`${firstName} ${lastName}`}
           </h2>
-          <p className='mt-1 text-sm leading-6 text-gray-400'>Zaman Dilimi</p>
+          <p className='mt-1 text-sm leading-6 text-gray-400'>{timezone}</p>
         </div>
 
         <form className='md:col-span-2'>
@@ -29,7 +37,7 @@ export default function PersonalInfo() {
                   id='first-name'
                   autoComplete='given-name'
                   onChange={(event) => {
-                    console.log(event.target.value)
+                    setFirstName(event.target.value);
                   }}
                   className='block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6'
                 />
@@ -50,6 +58,7 @@ export default function PersonalInfo() {
                   id='last-name'
                   autoComplete='family-name'
                   className='block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6'
+                  onChange={(e)=>setLastName(e.target.value)}
                 />
               </div>
             </div>
@@ -66,6 +75,7 @@ export default function PersonalInfo() {
                   id='timezone'
                   name='timezone'
                   className='block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 [&_*]:text-black'
+                  onChange={(e)=>setTimezone(e.target.value)}
                 >
                   <option>Pasifik Standart Saati</option>
                   <option>Doğu Standart Saati</option>
@@ -77,6 +87,7 @@ export default function PersonalInfo() {
 
           <div className='mt-8 flex'>
             <button
+              disabled={!isFormComplete}
               type='submit'
               className='rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500'
             >
